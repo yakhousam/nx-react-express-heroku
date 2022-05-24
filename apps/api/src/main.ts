f5f35f0cad5804ee0022227cc1ee35a71966b8d4
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-05-24 10:54:27 +0100

    added deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,11 +4,18 @@
  */
 
 import * as express from 'express';
+import * as path from 'path';
 
+const CLIENT_BUILD_PATH = path.join(__dirname, '../todos');
 const app = express();
+app.use(express.static(CLIENT_BUILD_PATH));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
+});
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
 const port = process.env.port || 3333;
