f84377fb90778463ecba3f96fe204d6ba586908f
yakhousam <42283612+yakhousam@users.noreply.github.com>
2022-05-24 11:22:14 +0100

    capitalized express port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -18,7 +18,7 @@ app.get('*', (request, response) => {
   response.sendFile(path.join(CLIENT_BUILD_PATH, 'index.html'));
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
